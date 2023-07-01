9821c9e84f4c5395f78951daf86fc0c6447b8329
Romeo Obeng <robeng962@gmail.com>
2023-07-01 01:24:10 +0000

    added ability to load kernel/read sectors into memory

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -15,17 +15,17 @@ start:
 
 init:
     cli                  ; Clear interrupts (disable interrupts)
-    mov ax, 0x00         ; Move the value 0x7C0 into the AX register
+    mov ax, 0x00         ; Move the value 0x7C0 into the AX register (set segment registers)
     mov ds, ax           ; Move the value in AX into the DS register
     mov es, ax           ; Move the value in AX into the ES register
     mov ss, ax           ; Move the value in AX into the SS register
-    mov sp, 0x7c00       ; Move the value 0x7C00 into the SP register
+    mov sp, 0x7c00       ; Move the value 0x7C00 into the SP register (stack pointer)
     sti                  ; Enable interrupts (enable interrupts)
 
 .load_protected:
     cli                  ; Clear interrupts (disable interrupts)
-    lgdt [gdt_descriptor] ; Load the GDT descriptor into GDTR
-    mov eax, cr0         ; Move the value of CR0 into EAX
+    lgdt [gdt_descriptor] ; Load the GDT descriptor into GDTR (Global Descriptor Table Register)
+    mov eax, cr0         ; Move the value of CR0 (Control Register 0) into EAX
     or eax, 0x1          ; Set the first bit of EAX to enable protected mode
     mov cr0, eax         ; Move the value in EAX back to CR0
     jmp CODE_SEG:load32  ; Far jump to the protected mode code segment
@@ -69,37 +69,73 @@ gdt_descriptor:
     dd gdt_start                ; Address of the GDT
 
 [BITS 32]
-load32:
-    mov ax, DATA_SEG     ; Move the offset of the data segment into AX
-    mov ds, ax           ; Load DS with the data segment offset
-    mov es, ax           ; Load ES with the data segment offset
-    mov fs, ax           ; Load FS with the data segment offset
-    mov gs, ax           ; Load GS with the data segment offset
-    mov ss, ax           ; Load SS with the data segment offset
-    mov ebp, 0x00200000  ; Set EBP to 0x00200000
-    mov esp, ebp         ; Set ESP to the value in EBP
+_lodk:
+    mov eax, 1 ; Start reading from sector 1 because sector 2 is used for the bootloader
+    mov ecx, 100 ; Number of sectors to load
+    mov edi, 0x0100000 ; Destination address in memory to load the sectors
+    call ata_lba_read ; Call the function to read sectors using ATA LBA (Logical Block Addressing) method
+    jpm CODE_SEG:0x0100000 ; Jump to the loaded code(the kernel) at memory address 0x0100000
 
-    ; The A20 line, also known as Address line 20, is a signal line in the x86 architecture that controls the addressing of memory beyond the 1 MB limit. 
-    ; In the real mode of operation (16-bit mode), the x86 processor can only directly address 20 bits of memory, allowing access to a maximum of 1 MB of physical memory.
-    ; The A20 line determines whether the address bus wraps around after reaching the 1 MB boundary or continues addressing the memory above 1 MB.
-    ; By default, the A20 line is disabled in real mode, which means that addresses greater than 1 MB will wrap around to the lower memory region.
-    ; Enabling the A20 line is necessary when transitioning from real mode to protected mode, as it allows the processor to access memory beyond 1 MB. 
-    ; Fast A20 Gate: On most newer computers starting with the IBM PS/2, the chipset has a FAST A20 option that can quickly enable the A20 line. 
-    ; To enable A20 this way, there is no need for delay loops or polling, just 3 simple instructions
-    ; ```
-    ;     in al, 0x92     ; Read the value from port 0x92 into AL
-    ;     or al, 2        ; Set bit 1 of AL
-    ;     out 0x92, al    ; Write the value in AL back to port 0x92
-    ; ```
-    ; These instructions read the value from port 0x92 into the AL register, which is a configuration port for various system functions. 
-    ; Bit 1 of the value is then set to enable the A20 line. Finally, the modified value is written back to port 0x92 to enable the A20 line.
-    ; Enabling the A20 line is necessary to properly access memory beyond 1 MB in protected mode, allowing the operating system and applications to utilize the full memory capacity of the system.
-    ; See https://wiki.osdev.org/A20_Line
-    in al, 0x92          ; Read the value from port 0x92 into AL
-    or al, 2             ; Set bit 1 of AL
-    out 0x92, al         ; Write the value in AL back to port 0x92
-    
-    jmp $                ; Endless loop (halt execution)
+; Function to read sectors using ATA LBA (Logical Block Addressing) method
+ata_lba_read:
+    mov ebx, eax ; Backup the LBA (Logical Block Address)
+
+    ; Send the highest 8 bits of the LBA to the hard disk controller
+    shr eax, 24 ; Shift right the LBA value by 24 bits to get the highest 8 bits
+    or eax, 0xE0 ; Set the highest 3 bits of the LBA to select the master drive
+    mov dx, 0x1F6 ; I/O port address for sending the command
+    out dx, al ; Send the value in AL (highest 8 bits of the LBA) to the I/O port
+    ; Finished sending the highest 8 bits of the LBA
+
+    ; Send the total sectors to read
+    mov eax, ecx ; Move the number of sectors to read into EAX
+    mov dx, 0x1F2 ; I/O port address for sending the total sectors
+    out dx, al ; Send the value in AL (total sectors) to the I/O port
+    ; Finished sending the total sectors to read
+
+    ; Send more bits of the LBA
+    mov eax, ebx ; Restore the backup LBA
+    mov dx, 0x1F3 ; I/O port address for sending more bits of the LBA
+    out dx, al ; Send the value in AL (8-15 bits of the LBA) to the I/O port
+    ; Finished sending more bits of the LBA
+
+    ; Send more bits of the LBA
+    mov dx, 0x1F4 ; I/O port address for sending more bits of the LBA
+    mov eax, ebx ; Restore the backup LBA
+    shr eax, 8 ; Shift right the LBA value by 8 bits to get the next 8 bits
+    out dx, al ; Send the value in AL (16-23 bits of the LBA) to the I/O port
+    ; Finished sending more bits of the LBA
+
+    ; Send upper 16 bits of the LBA
+    mov dx, 0x1F5 ; I/O port address for sending the upper 16 bits of the LBA
+    mov eax, ebx ; Restore the backup LBA
+    shr eax, 16 ; Shift right the LBA value by 16 bits to get the upper 16 bits
+    out dx, al ; Send the value in AL (24-31 bits of the LBA) to the I/O port
+    ; Finished sending upper 16 bits of the LBA
+
+    mov dx, 0x1f7 ; I/O port address for sending the read command
+    mov al, 0x20 ; Value 0x20 indicating the read command
+    out dx, al ; Send the read command to the I/O port
+
+; Read all sectors into memory
+.next_sector:
+    push ecx ; Preserve ECX value (number of sectors to read)
+
+; Checking if we need to read
+.try_again:
+    mov dx, 0x1f7 ; I/O port address for reading the status register
+    in al, dx ; Read the value from the I/O port into AL
+    test al, 8 ; Test the value in AL for bit 3 (BSY - Busy)
+    jz .try_again ; Jump back to .try_again label if BSY bit is set (device is busy)
+
+    ; We need to read 256 words (512 bytes) at a time
+    mov ecx, 256 ; Set ECX to 256
+    mov dx, 0x1F0 ; I/O port address for reading the data register
+    rep insw ; Repeat the input operation of a word (16 bits) from the I/O port
+    pop ecx ; Restore the original ECX value
+    loop .next_sector ; Repeat the loop for the remaining sectors
+    ; End of reading sectors into memory
+    ret ; Return from the function
 
 times 510-($ - $$) db 0  ; Fill the remaining bytes in the boot sector with zeros to make it 512 bytes
 
